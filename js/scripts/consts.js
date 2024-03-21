6b0934cfd5e2e4a5ed1f69a379d16473c8746a7a
AlexsandroFde <alexsandro.ferreira02@aluno.ifce.edu.br>
2024-03-20 23:54:46 -0300

    added consts js

---
+++ js/scripts/consts.js
@@ -0,0 +1,7 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTaskButton');
+const taskList = document.getElementById('taskList');
+const allTasksButton = document.getElementById('allTasksButton');
+const pendingTasksButton = document.getElementById('pendingTasksButton');
+const completedTasksButton = document.getElementById('completedTasksButton');
+const clearAllButton = document.getElementById('clearAllButton');
